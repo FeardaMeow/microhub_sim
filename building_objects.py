8a8f632f77b6c24b0adc67928b9c3460a2f9e314
FeardaMeow <shwang1121@gmail.com>
2020-05-21 17:34:06 -0700

    Updated building_objects.py

--- building_objects.py
+++ building_objects.py
@@ -1,13 +1,16 @@
 import simpy
 
-class Buidling():
+class Building():
 
     def __init__(self, env, num_residents):
         self.env = env
         self.num_residents = num_residents
 
-    def process_delivery(self, num_packages):
+    def process_delivery(self, num_packages, **kwargs):
         '''
         Generic Delivery Process: Make Delivery, Return to Vehicle
         '''
+        self._deliver_packages(num_packages, **kwargs)
+
+    def _deliver_packages(self, num_packages,**kwargs):
         pass
